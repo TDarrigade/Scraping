0e051ebe9064d19c020ee5c1a2fd42368a7a42f5
Thomas 2 <thomas2@MacBook-Pro-de-Thomas.local>
2018-01-18 17:02:56 +0100

    fusion des ruby

--- get_crypto.rb
+++ get_crypto.rb
@@ -2,8 +2,23 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
+def get_crypto_price()
+	page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+	
+	prices = []
 
-def get_btc_course()
+	page.xpath('//a[@class="price"]').each do |price|
+		prices << price.text.gsub(' ','')
+
+		#prices[0].gsub("\n","")
+	end
+	 #puts "1 bitcoin vaut : #{prices[0..10]}"
+	 #sleep(5)
+	 
+end
+
+
+def get_crypto_name()
 	page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
 	
 	prices = []
@@ -13,10 +28,11 @@ def get_btc_course()
 
 		#prices[0].gsub("\n","")
 	end
-	 puts "1 bitcoin vaut : #{prices[0..10]}"
+	 #puts "Les crypto s'appelle : #{prices[0..10]}"
 	 #sleep(5)
 end
 
 
 #loop { get_btc_course }
-get_btc_course
+#get_btc_course
+
